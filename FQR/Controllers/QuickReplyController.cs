1429b5272d1917f806fa5c4b9633cb8eee269319
limky2277 <limkahyuan1996@gmail.com>
2020-05-22 22:55:22 +0800

    Add project files.

---
+++ FQR/Controllers/QuickReplyController.cs
@@ -0,0 +1,116 @@
+﻿using FQR.Models;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace FQR.Controllers
+{
+    #region References
+    /*
+     * https://github.com/eternaltung/MessengerBot-WebAPI
+     * https://glitch.com/edit/#!/daffodil-authorization?path=bot.js%3A1154%3A23
+    */
+    #endregion
+    public class QuickReplyController : ApiController
+    {
+        string pageToken = "";
+        string appSecret = "";
+
+        public HttpResponseMessage Get()
+        {
+            var querystrings = Request.GetQueryNameValuePairs().ToDictionary(x => x.Key, x => x.Value);
+            if (querystrings["hub.verify_token"] == "facebookverifytokenhere")
+            {
+                return new HttpResponseMessage(HttpStatusCode.OK)
+                {
+                    Content = new StringContent(querystrings["hub.challenge"], Encoding.UTF8, "text/plain")
+                };
+            }
+            return new HttpResponseMessage(HttpStatusCode.Unauthorized);
+        }
+
+        [HttpPost]
+        public async Task<HttpResponseMessage> Post()
+        {
+            var signature = Request.Headers.GetValues("X-Hub-Signature").FirstOrDefault().Replace("sha1=", "");
+            var body = await Request.Content.ReadAsStringAsync();
+            if (!VerifySignature(signature, body))
+                return new HttpResponseMessage(HttpStatusCode.BadRequest);
+
+            var value = JsonConvert.DeserializeObject<WebhookModel>(body);
+            if (value._object != "page")
+                return new HttpResponseMessage(HttpStatusCode.OK);
+
+            foreach (var item in value.entry[0].messaging)
+            {
+                if (item.message == null && item.postback == null)
+                    continue;
+                else
+                    await SendMessage(GetMessageTemplate(item.message.text, item.sender.id));
+            }
+
+            return new HttpResponseMessage(HttpStatusCode.OK);
+        }
+
+        private bool VerifySignature(string signature, string body)
+        {
+            var hashString = new StringBuilder();
+            using (var crypto = new HMACSHA1(Encoding.UTF8.GetBytes(appSecret)))
+            {
+                var hash = crypto.ComputeHash(Encoding.UTF8.GetBytes(body));
+                foreach (var item in hash)
+                    hashString.Append(item.ToString("X2"));
+            }
+
+            return hashString.ToString().ToLower() == signature.ToLower();
+        }
+
+        /// <summary>
+        /// get text message template
+        /// </summary>
+        /// <param name="text">text</param>
+        /// <param name="sender">sender id</param>
+        /// <returns>json</returns>
+        private JObject GetMessageTemplate(string text, string sender)
+        {
+            // Quick reply 
+            var quickReplyOption = new List<QuickReplyOption>
+            {
+                new QuickReplyOption { title = "Test A", payload = "Test A Payload" },
+                new QuickReplyOption { title = "Test B", payload = "Test B Payload" },
+                new QuickReplyOption { title = "Test C", payload = "Test C Payload" },
+            };
+
+
+            return JObject.FromObject(new
+            {
+                recipient = new { id = sender },
+                message = new { text = text, quick_replies = quickReplyOption }
+            });
+        }
+
+        /// <summary>
+        /// send message
+        /// </summary>
+        /// <param name="json">json</param>
+        private async Task SendMessage(JObject json)
+        {
+            // {{ "recipient": { "id": "" }, "message": { "text": "Test" } }}
+            // {{ "recipient": { "id": "" }, "message": { "text": "Test", "quick_replies": [ { "content_type": "text", "title": "Test A", "payload": null }, { "content_type": "text", "title": "Test B", "payload": null }, { "content_type": "text", "title": "Test C", "payload": null } ] } }}
+            using (HttpClient client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                HttpResponseMessage res = await client.PostAsync($"https://graph.facebook.com/v2.6/me/messages?access_token={pageToken}", new StringContent(json.ToString(), Encoding.UTF8, "application/json"));
+            }
+        }
+    }
+}
