c34c0195796ea268497b6af12dc9b0c9fdf5d490
limky2277 <limkahyuan1996@gmail.com>
2020-06-14 16:34:12 +0800

    Added static HttpClient

---
+++ TicketBOT/Helpers/RestApiHelper.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace TicketBOT.Helpers
+{
+    public class RestApiHelper
+    {
+        private static HttpClient _client;
+
+        static RestApiHelper()
+        {
+            _client = new HttpClient();
+        }
+
+        public static Task<HttpResponseMessage> GetAsync(string requestUri)
+        {
+            return _client.GetAsync(requestUri);
+        }
+
+        public static Task<HttpResponseMessage> PostAsync(string requestUri, HttpContent content)
+        {
+            return _client.PostAsync(requestUri, content);
+        }
+    }
+}
--- TicketBOT/Services/FacebookServices/FbApiClientService.cs
+++ TicketBOT/Services/FacebookServices/FbApiClientService.cs
@@ -4,6 +4,7 @@ using System.Net;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using TicketBOT.Helpers;
 using TicketBOT.Models;
 using TicketBOT.Models.Facebook;
 using TicketBOT.Services.Interfaces;
@@ -12,7 +13,7 @@ namespace TicketBOT.Services.FacebookServices
 {
     public class FbApiClientService : IFbApiClientService
     {
-        private readonly HttpClient _client;
+        private static HttpClient _client;
         private readonly ApplicationSettings _appSettings;
 
         public FbApiClientService(HttpClient client, ApplicationSettings appSettings)
@@ -23,7 +24,7 @@ namespace TicketBOT.Services.FacebookServices
 
         public async Task<FacebookSender> GetUserInfoAsync(string pageToken, string senderId)
         {
-            var resp = await _client.GetAsync(string.Format(_appSettings.FacebookGraphApiEndpoint.GetProfile, senderId, pageToken));
+            var resp = await RestApiHelper.GetAsync(string.Format(_appSettings.FacebookGraphApiEndpoint.GetProfile, senderId, pageToken));
             if (resp.StatusCode == HttpStatusCode.OK)
             {
                 return JsonConvert.DeserializeObject<FacebookSender>(resp.Content.ReadAsStringAsync().Result);
@@ -34,7 +35,7 @@ namespace TicketBOT.Services.FacebookServices
 
         public async Task PostMessageAsync(string pageToken, JObject json)
         {
-            await _client.PostAsync(string.Format(_appSettings.FacebookGraphApiEndpoint.PostMessage, pageToken), new StringContent(json.ToString(), Encoding.UTF8, "application/json"));
+            await RestApiHelper.PostAsync(string.Format(_appSettings.FacebookGraphApiEndpoint.PostMessage, pageToken), new StringContent(json.ToString(), Encoding.UTF8, "application/json"));
         }
     }
 }
